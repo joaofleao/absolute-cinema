ab9f0218bd8ef7a30f3ed98c13b844e838180e75
João Flores de Leão <joaofleao@gmail.com>
2025-08-19 08:30:00 -0300

    util functions

--- src/utils/print.ts
+++ src/utils/print.ts
@@ -1,15 +1,15 @@
 const print = (
   api: string,
   message: string,
-  color: 'blue' | 'red' | 'yellow' | 'green' | number
+  color: 'blue' | 'red' | 'yellow' | 'green' | number,
 ): void => {
-  if (color === 'blue') color = 34;
-  if (color === 'red') color = 31;
-  if (color === 'yellow') color = 33;
-  if (color === 'green') color = 32;
+  if (color === 'blue') color = 34
+  if (color === 'red') color = 31
+  if (color === 'yellow') color = 33
+  if (color === 'green') color = 32
 
   // eslint-disable-next-line no-console
-  console.log(`\x1b[${color}m${api} \x1b[0m- ${message}`);
-};
+  console.log(`\x1b[${color}m${api} \x1b[0m- ${message}`)
+}
 
-export default print;
+export default print
