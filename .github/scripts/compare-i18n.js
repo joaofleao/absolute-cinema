ff0dbbe5f2ce55ef98a3dc04ec8c24b37c3ab2f6
João Flores de Leão <joaofleao@gmail.com>
2025-08-20 10:16:18 -0300

    update i18n action

--- .github/scripts/compare-i18n.js
+++ .github/scripts/compare-i18n.js
@@ -1,7 +1,10 @@
-import fs from 'fs'
-import path from 'path'
+import { readFileSync } from 'fs'
+import { dirname, resolve, join } from 'path'
+import { fileURLToPath } from 'url'
 
-const getKeys = (obj, prefix = '') => {
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+function getKeys(obj, prefix = '') {
   return Object.keys(obj).flatMap((key) => {
     const value = obj[key]
     const fullKey = prefix ? `${prefix}.${key}` : key
@@ -12,9 +15,9 @@ const getKeys = (obj, prefix = '') => {
   })
 }
 
-const basePath = path.resolve(__dirname, '../../src/i18n/locales')
-const en = JSON.parse(fs.readFileSync(path.join(basePath, 'en-us.json'), 'utf8'))
-const pt = JSON.parse(fs.readFileSync(path.join(basePath, 'pt-br.json'), 'utf8'))
+const basePath = resolve(__dirname, '../../src/i18n/locales')
+const en = JSON.parse(readFileSync(join(basePath, 'en-us.json'), 'utf8'))
+const pt = JSON.parse(readFileSync(join(basePath, 'pt-br.json'), 'utf8'))
 
 const enKeys = getKeys(en).sort()
 const ptKeys = getKeys(pt).sort()
@@ -24,15 +27,12 @@ const extraInPt = ptKeys.filter((k) => !enKeys.includes(k))
 
 if (missingInPt.length || extraInPt.length) {
   if (missingInPt.length) {
-    // eslint-disable-next-line no-console
     console.error('Missing keys in pt-br.json:', missingInPt)
   }
   if (extraInPt.length) {
-    // eslint-disable-next-line no-console
     console.error('Extra keys in pt-br.json:', extraInPt)
   }
   process.exit(1)
 } else {
-  // eslint-disable-next-line no-console
   console.log('i18n files have matching keys!')
 }
