32448c66fea0d7cbdb6867036890866ece81fc73
João Flores de Leão <joaofleao@gmail.com>
2026-07-07 22:52:35 -0300

    current

--- src/router/types.ts
+++ src/router/types.ts
@@ -1,14 +1,12 @@
 import { GenericId } from 'convex/values'
 
-import { BottomTabScreenProps } from '@react-navigation/bottom-tabs'
+// import { BottomTabScreenProps } from '@react-navigation/bottom-tabs'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
 export type StackProps = {
-  home: undefined
+  movies: undefined
   profile: undefined
-  watchlist: undefined
-  watched: undefined
-  onboarding: undefined
+  search: undefined
 
   movie: undefined
   password_recovery: undefined
@@ -32,12 +30,13 @@ export type StackProps = {
       tagline?: string
     }
   }
-  search: undefined
+
   auth: undefined
+  onboarding: undefined
 }
 
 export type ScreenProps<T extends keyof StackProps> = NativeStackScreenProps<StackProps, T>
 export type ScreenType<T extends keyof StackProps> = (props: ScreenProps<T>) => React.ReactElement
 
-export type TabProps<T extends keyof StackProps> = BottomTabScreenProps<StackProps, T>
-export type TabType<T extends keyof StackProps> = (props: TabProps<T>) => React.ReactElement
+// export type TabProps<T extends keyof StackProps> = BottomTabScreenProps<StackProps, T>
+// export type TabType<T extends keyof StackProps> = (props: TabProps<T>) => React.ReactElement
